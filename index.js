a85f15b8aa1a31f20d97c8fcb76ac2dd4571c630
Breno Estrella <brenogbestrella@gmail.com>
2021-03-03 21:46:55 -0300

    Adicionado exemplos

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const app = express();
+const listaUsuarios = [
+    'Breno',
+    'Ivens',
+    'Jailson',
+    'Nykolle',
+];
 
-app.get('/', (req,res) => {
-    return res.send('Hello world')   //usar return como boa prática
+//Query Params
+//Ex:localhost:12345/usuarios?id=1
+app.get('/usuarios', (req, res) => {
+    const { nome } = req.query;
+    let listaRetorno = listaUsuarios.filter(i => i.includes(nome || ''))
+    return res.json(listaRetorno)
 });
 
-app.listen(3000, () => {
-    console.log('Servidor rodando na porta 3000')
+
+//Route Params
+//Ex: ecommer.com.br/produtos/4/detalhes
+app.get('/hello/:usuario', (req,res) => {
+    const { usuario } = req.params;
+    return res.send(`Hello world ${usuario}`);   //usar return como boa prática
+});
+
+
+app.listen(12345, () => {
+    console.log('Servidor rodando na porta 12345')
 });
